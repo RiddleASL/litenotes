2b64b7a1e36f31c40295888ad8f492002543b712
RiddleASL <n00212142@iadt.ie>
2022-10-23 23:39:20 +0100

    View notes feature added, branch created. 23/10/22

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -1,23 +1,27 @@
 <x-app-layout>
-    <x-slot>
-        <h2>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ ('Notes')}}
         </h2>
     </x-slot>
 
-    <div class="" style="padding-top: 2rem">
-        <a href="{{route('notes.create')}}" style="padding: 1rem; background-color: white;"> +New Note</a>
-        <br>
-        <br>
-        @forelse ($notes as $note)
-            <a href="{{route('notes.show', $note->uuid)}}" style="font-weight: bold"><u>{{ $note->title }}</u></a>
-            <p style="width: 70%;">
-                {{Str::limit($note->text, 400)}}
-            </p>
-            <span style="font-size: .75rem"><i>{{$note->created_at->diffForHumans()}}</i></span>
-        @empty
-        <p>You have no notes</p>
-        @endforelse
-        {{$notes->links()}}
+    <div class="py-12">
+        <div class="max-w-7x1 mx-auto sm:px-6 lg:px-8">
+            <a href="{{route('notes.create')}}" class="btn-link btn-lg mb-2"> + New Note</a>
+            @forelse ($notes as $note)
+                <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
+                    <h2 class="font-bold text-2x1">
+                        <a href="{{route('notes.show', $note->uuid)}}"><u>{{ $note->title }}</u></a>
+                    </h2>
+                    <p class="mt-2">
+                        {{Str::limit($note->text, 400)}}
+                    </p>
+                    <span class="block mt-4 text-sm opacity-70"><i>{{$note->created_at->diffForHumans()}}</i></span>
+                </div>
+            @empty
+                <p>You have no notes</p>
+            @endforelse
+            {{$notes->links()}}
+        </div>
     </div>
 </x-app-layout>
